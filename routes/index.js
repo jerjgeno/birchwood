4a87e065c948da4ec44f4bb4dbe6def4f0cb33ab
jerj <johnbrz@gmail.com>
2019-03-28 20:54:36 +0000

    Site looking good, simple, side nav works, design is clean

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Birchwood',
-    subtitle: 'Digital Media Agency', 
+  res.render('john', { 
+  });
+});
+
+router.get('/john', function(req, res, next) {
+  res.render('john', { 
   });
 });
 
