a58fcee6c1319e3dd9e083062134267755804c0a
jerj <johnbrz@gmail.com>
2019-03-29 17:42:07 +0000

    Added footer

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.locals.config = {
+  year: new Date().getFullYear() 
+}; 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
